9c9151c4a545e60a02c3c06b7083b06b3d23be21
Toothyca <Toothyca@gmail.com>
2024-10-02 21:18:53 -0700

    rudimentary flashcard retrieval and display

---
+++ main_page.py
@@ -0,0 +1,15 @@
+import sqlite3
+
+conn = sqlite3.connect("tsf.db")
+cur = conn.cursor()
+
+cur.execute("SELECT * FROM flashcards")
+
+card_num = 1
+for row in cur:
+    character, pinyin, meaning, lesson, mastered = row[0], row[1], row[2], row[3], row[4]
+    print("------Card: {0}------\nCharacter: {1[0]}\nPinyin: {1[1]}\nMeaning: {1[2]}\nLesson: {1[3]}\nMastered: {1[4]}".format(card_num, row))
+    card_num+=1
+
+cur.close()
+conn.close()
